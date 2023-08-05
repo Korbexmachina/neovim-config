6aa20c760e621547a1aa2943b939b2bb7ab90e5b
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-08-04 23:12:28 -0700

    migrating to rose-pine theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,10 +1,13 @@
 return {
       {
-    -- nightfox theme
-    'EdenEast/nightfox.nvim',
+    -- rose pine theme
+    'rose-pine/neovim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'nightfox'
+      require('rose-pine').setup({
+        variant = 'main'
+      })
+      vim.cmd.colorscheme 'rose-pine'
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
       vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
     end,
